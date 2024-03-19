1d1e642e5ec267e96fb64a007a3c5c7e6848d411
Reut Vladislav <reut-2011@mail.ru>
2024-03-19 18:58:39 +0300

    feat: add function memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -117,8 +117,14 @@ function getPolynom(a, b, c) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let result;
+  return () => {
+    if (!result) {
+      result = func();
+    }
+    return result;
+  };
 }
 
 /**
